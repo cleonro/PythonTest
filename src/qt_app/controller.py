d376f447e68f38bebf4b371ccd039d5e905c13a9
catalin.ciobirca <catalin.ciobirca@yahoo.com>
2026-04-23 12:41:53 +0300

    Added simple tests.

--- src/qt_app/controller.py
+++ src/qt_app/controller.py
@@ -20,7 +20,6 @@ class Controller(QObject):
         super().__init__()
         self._initialized = True
         self._actions = []
-        self._build_actions()
 
     def actions(self):
         return tuple(self._actions)
@@ -41,18 +40,7 @@ class Controller(QObject):
         if action in self._actions:
             self._actions.remove(action)
             self.action_removed.emit(action)
-
-    def _build_actions(self):
-        say_hello_action = QAction("&Say Hello", self)
-        say_hello_action.setStatusTip("Show a greeting dialog")
-        say_hello_action.triggered.connect(self.say_hello)
-
-        plot_demo_action = QAction("&Plot Demo", self)
-        plot_demo_action.setStatusTip("Open the sample matplotlib plot")
-        plot_demo_action.triggered.connect(self.plot_demo)
-
-        self.add_action(say_hello_action)
-        self.add_action(plot_demo_action)
+   
 
     def _main_window(self):
         window = QApplication.activeWindow()
@@ -60,25 +48,3 @@ class Controller(QObject):
             return window
         return None
 
-    @Slot()
-    def say_hello(self):
-        window = self._main_window()
-        if window is not None:
-            window.statusBar().showMessage("Hello from the controller")
-        QMessageBox.information(window, "Hello", "Hello from the Actions menu")
-
-    @Slot()
-    def plot_demo(self):
-        window = self._main_window()
-        if window is not None:
-            window.statusBar().showMessage("Opening plot demo")
-        self._show_plot()
-
-    def _show_plot(self):
-        import matplotlib.pyplot as plt
-        import numpy
-
-        x = numpy.random.normal(5.0, 3.0, 100000)
-        y = numpy.sin(x)
-        plt.plot(x, y)
-        plt.show()
--- src/qt_app/simple_test.py
+++ src/qt_app/simple_test.py
@@ -5,10 +5,10 @@ from PySide6.QtGui import QAction
 # def test_matplotlib():
 #     import matplotlib.pyplot as plt
 #     import numpy as np
-#
+
 #     x = np.linspace(0, 10, 100)
 #     y = np.sin(x)
-#
+
 #     plt.plot(x, y)
 #     plt.title("Sine Wave")
 #     plt.xlabel("x")
@@ -26,9 +26,9 @@ def test_matplotlib():
     import matplotlib.pyplot as plt
 
     x = numpy.random.normal(5.0, 3.0, 100000)
-    y = numpy.sin(x)
-    # plt.hist(x, 100)
-    plt.plot(x, y)
+    # y = numpy.sin(x)
+    plt.hist(x, 100)
+    # plt.plot(x, y)
     plt.show()
 
 
@@ -36,3 +36,47 @@ action_test_matplotlib = QAction("Test Matplotlib")
 action_test_matplotlib.triggered.connect(test_matplotlib)
 
 Controller().add_action(action_test_matplotlib)
+
+
+def test_numpy():
+    import numpy as np
+
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5, 6])
+    c = a + b
+    print("Result of adding two arrays:", c)
+
+action_test_numpy = QAction("Test NumPy")
+action_test_numpy.triggered.connect(test_numpy)
+Controller().add_action(action_test_numpy)
+
+
+def test_pandas():
+    import pandas as pd
+
+    data = {
+        "Name": ["Alice", "Bob", "Charlie"],
+        "Age": [25, 30, 35],
+        "City": ["New York", "Los Angeles", "Chicago"]
+    }
+    df = pd.DataFrame(data)
+    print("DataFrame:\n", df)
+
+action_test_pandas = QAction("Test Pandas")
+action_test_pandas.triggered.connect(test_pandas)
+Controller().add_action(action_test_pandas)
+
+
+def test_torch():
+    import torch
+
+    x = torch.rand(2, 2)
+    y = torch.rand(2, 2)
+    print("Tensor x:\n", x)
+    print("Tensor y:\n", y)
+    z = x + y
+    print("Result of adding two tensors:\n", z)
+
+action_test_torch = QAction("Test Torch")
+action_test_torch.triggered.connect(test_torch)
+Controller().add_action(action_test_torch)
